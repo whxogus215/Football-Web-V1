9d6cca791fcf12bc9abd00609a8bb77e0172a583
whxogus215 <whxogus215@naver.com>
2023-08-04 17:27:37 +0900

    Vue JS 공식문서 연습

---
+++ V1/frontend/src/components/ToDoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="checkbox" :id="id" :checked="isDone" />
+    <label :for="id">{{label}}</label>
+  </div>
+</template>
+
+<script>
+import uniqueId from "lodash.uniqueid";
+
+export default {
+  name: "ToDoItem",
+  props: {
+    label: { required : true, type : String},
+    done: { default : false, type : Boolean}
+  },
+  data() { // Vue JS에서 사용되는 데이터 보관함 (Object 자료형)
+    return {
+      id: 'todo-' + uniqueId(),
+      isDone : this.done,
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+
+</style>

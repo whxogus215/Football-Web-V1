8b70096d0d0ad59b334ca279db3b29ff7f774dd8
whxogus215 <whxogus215@naver.com>
2023-08-07 22:43:11 +0900

    Feat : Vue Item List

--- V1/frontend/src/App.vue
+++ V1/frontend/src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    <h1>To-Do List</h1>
-    <ul>
+    <h1>My To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary">{{listSummary}}</h2>
+    <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
             :label="item.label"
             :done="item.done"
-            :id="item.id"></to-do-item>
+            :id="item.id"
+            @checkbox-changed="updateDoneStatus(item.id)"
+            @item-deleted="deleteToDo(item.id)"
+            @item-edited="editToDo(item.id, $event)"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
+<!-- App.vue는 실제 웹 사이트에서 사용되는 값들에 대한 로직을 수행한다. 그리고 컴포넌트는 각각 맡은 분야에 대한 이벤트 발생 및 함수를 호출한다. -->
 
 <script>
 import ToDoItem from "@/components/ToDoItem.vue";
 import uniqueId from "lodash.uniqueid";
+import ToDoForm from "./components/ToDoForm";
 
 export default {
   name: "app",
   components: {
-    ToDoItem,
+    ToDoItem, ToDoForm,
   },
   data() {
     return {
@@ -34,17 +41,142 @@ export default {
         { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
       ]
     }
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done:false});
+    },
+    updateDoneStatus(toDoId) {
+      const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId)
+      toDoToUpdate.done = !toDoToUpdate.done
+    },
+    deleteToDo(toDoId) {
+      const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
+      this.ToDoItems.splice(itemIndex, 1);
+    },
+    editToDo(toDoId, newLabel) {
+      const toDoToEdit = this.ToDoItems.find((item) => item.id === toDoId);
+      toDoToEdit.label = newLabel;
+    }
+  },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter((item) => item.done).length
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* Global styles */
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
+}
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+.btn__filter {
+  border-color: lightgrey;
+}
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-group > * {
+  flex: 1 1 auto;
+}
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+[class*="stack"] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow:
+      0 2px 4px 0 rgba(0, 0, 0, 0.2),
+      0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
 }
 </style>
